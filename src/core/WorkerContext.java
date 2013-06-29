6b567c2ede426abdbf78cf676230414a5c8167ad
Bruce King <xieweiking@gmail.com>
2013-06-29 17:16:08 +0800

    Finish WorkContext attribute support

--- src/core/WorkerContext.java
+++ src/core/WorkerContext.java
@@ -7,9 +7,9 @@ import java.util.Map;
 
 public abstract class WorkerContext<BCTX extends BossContext<?>, BOSS extends Boss<?, ?, BCTX, BOSS>> {
 
-    private final BOSS boss;
-    private final DatagramChannel channel;
-    private final BCTX context;
+    private final BOSS                boss;
+    private final DatagramChannel     channel;
+    private final BCTX                context;
     private final Map<String, Object> attributes;
 
     public WorkerContext(final BOSS boss, final DatagramChannel channel, final BCTX context) {
@@ -31,12 +31,23 @@ public abstract class WorkerContext<BCTX extends BossContext<?>, BOSS extends Bo
         return this.context;
     }
 
-    public boolean hasAttr(final String key) {
+    public boolean hasAttribute(final String key) {
         return this.attributes.containsKey(key);
+    }
+
+    public boolean hasAttribute(final String key, final Class<?> clazz) {
+        return this.hasAttribute(key) && clazz.equals(this.getAttribute(key).getClass());
     }
 
     public Object getAttribute(final String key) {
         return this.attributes.get(key);
+    }
+
+    public <T> T getAttribute(final String key, final Class<T> clazz) {
+        if (this.hasAttribute(key, clazz)) {
+            return clazz.cast(this.getAttribute(key));
+        }
+        return null;
     }
 
     public Object setAttribute(final String key, final Object value) {
